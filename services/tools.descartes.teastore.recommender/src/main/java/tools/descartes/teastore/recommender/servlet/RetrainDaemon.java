08714eeb35de0e2729cb3996f8e49c626f884a55
Chao Zhan <chao.zhan@campus.tu-berlin.de>
2024-05-21 17:16:06 +0200

    fix: cleanup deps and close the client and response gracefully

--- services/tools.descartes.teastore.recommender/src/main/java/tools/descartes/teastore/recommender/servlet/RetrainDaemon.java
+++ services/tools.descartes.teastore.recommender/src/main/java/tools/descartes/teastore/recommender/servlet/RetrainDaemon.java
@@ -13,6 +13,9 @@
  */
 package tools.descartes.teastore.recommender.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tools.descartes.teastore.recommender.restclient.PersistenceClient;
 import tools.descartes.teastore.utils.RegistryClient;
 import tools.descartes.teastore.utils.Service;
 
@@ -22,6 +25,7 @@ import tools.descartes.teastore.utils.Service;
  * @author Johannes Grohmann
  */
 public class RetrainDaemon extends Thread {
+	private static final Logger LOG = LoggerFactory.getLogger(RetrainDaemon.class);
 	/**
 	 * The time between retraining in milliseconds.
 	 */
@@ -53,7 +57,7 @@ public class RetrainDaemon extends Thread {
 			try {
 				Thread.sleep(looptime);
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				LOG.warn("RetrainDaemon interrupted");
 			}
 			// wait for the persistance service and then retrain
 			RegistryClient.runAfterServiceIsAvailable(Service.PERSISTENCE.getServiceName(), () -> {
