b0e8cab63829963e03188f6000d557d928121944
Chao Zhan <chao.zhan@campus.tu-berlin.de>
2024-06-26 23:30:51 +0200

    feat(frontend): add global config

---
+++ services/tools.descartes.teastore.frontend/app/appConfig.tsx
@@ -0,0 +1,46 @@
+export default {
+  /**
+	 * Text for message cookie.
+	 */
+	MESSAGECOOKIE: "TeaStoreMessageCookie",
+	/**
+	 * Text for error message cookie.
+	 */
+	ERRORMESSAGECOOKIE: "TeaStoreErrorMessageCookie",
+	/**
+	 * Text for successful login. 
+	 */
+	SUCESSLOGIN: "You are logged in!",
+	/**
+	 * Text for logout.
+	 */
+	SUCESSLOGOUT: "You are logged out!",
+	/**
+	 * Text for wrong credentials.
+	 */
+	WRONGCREDENTIALS: "You used wrong credentials!",
+	/**
+	 * Text for number products cookie.
+	 */
+	PAGESIZECOOKIE: "TeaStorePageSize",
+	/**
+	 * Text for session blob.
+	 */
+	SESSIONBLOBCOOKIE: "SessionBlob",
+	/**
+	 * Text for confirmed order.
+	 */
+	ORDERCONFIRMED: "Your order is confirmed!",
+	/**
+	 * Text for updated cart.
+	 */
+	CARTUPDATED: "Your cart is updated!",
+	/**
+	 * Text for added product.
+	 */
+	ADDPRODUCT: "Product %s is added to cart!",
+	/**
+	 * Text for removed product.
+	 */
+	REMOVEPRODUCT: "Product %s is removed from cart!",
+}
