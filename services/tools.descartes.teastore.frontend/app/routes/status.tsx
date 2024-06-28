3be9124ea22d6fe32e5d72038548c7d4ae6b030c
Rui Zhao <rui.zhao@campus.tu-berlin.de>
2024-06-28 15:35:57 +0200

    add webui status page

--- services/tools.descartes.teastore.frontend/app/routes/status.tsx
+++ services/tools.descartes.teastore.frontend/app/routes/status.tsx
@@ -1,7 +1,31 @@
+// export default function StatusPage() {
+//   return (
+//     <div>
+//       Status
+//     </div>
+//   )
+// }
+
+
+
 export default function StatusPage() {
   return (
-    <div>
-      Status
+    <div className="container" id="main">
+      <div className="row">
+        <div className="col-sm-12 col-lg-8 col-lg-offset-2">
+          <h2 className="minipage-title">TeaStore Service Status</h2>
+          <br/>
+          <p><b>This page does not auto refresh!</b> Refresh manually or start an auto refresh for checking the current status (e.g. to see if database generation has finished).</p>
+          <p>
+            <b>Note:</b> Database and image generation may take a while.
+            Leave the TeaStore in a stable and unused state while the database is generating.
+            You may use the TeaStore once the database has finished generating.
+            Please wait for the image provider to finish as well before running any performance tests.
+          </p>
+          <br/>
+          {/* form and table here: to do */}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
