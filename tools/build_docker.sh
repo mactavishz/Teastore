878589d723ce581a230fac6fdbb572636e900fb8
Chao Zhan <chao.zhan@campus.tu-berlin.de>
2024-07-13 13:36:49 +0200

    build: update build_docker script with new db size config

--- tools/build_docker.sh
+++ tools/build_docker.sh
@@ -25,15 +25,28 @@ if [[ ! -z $tag ]]; then
 	tag=":${tag}"
 fi
 
+DB_SIZES=("small" "mid" "large")
+
+DB_PATH="../utilities/tools.descartes.teastore.database/"
+IMAGE_PATH="../services/tools.descartes.teastore.image/"
+PERSISTENCE_PATH="../services/tools.descartes.teastore.persistence/"
+WEBUI_PATH="../services/tools.descartes.teastore.webui/"
+AUTH_PATH="../services/tools.descartes.teastore.auth/"
+RECOMMENDER_PATH="../services/tools.descartes.teastore.recommender/"
+
 if [[ $DEPLOY = 1 ]]; then
 	docker run -it --rm --privileged tonistiigi/binfmt --install all
 	docker buildx create --use --name mybuilder
-	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-db${tag}" ../utilities/tools.descartes.teastore.database/ ${push_flag:+--push}
-	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-persistence${tag}" ../services/tools.descartes.teastore.persistence/ ${push_flag:+--push}
-	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-image${tag}" ../services/tools.descartes.teastore.image/ ${push_flag:+--push}
-	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-webui${tag}" ../services/tools.descartes.teastore.webui/ ${push_flag:+--push}
-	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-auth${tag}" ../services/tools.descartes.teastore.auth/ ${push_flag:+--push}
-	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-recommender${tag}" ../services/tools.descartes.teastore.recommender/ ${push_flag:+--push}
+
+  for size in "${DB_SIZES[@]}"
+  do
+    docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-db${tag}-db${size}" --build-arg DB_SIZE=${size} ${DB_PATH} ${push_flag:+--push}
+    docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-image${tag}-db${size}" --build-arg DB_SIZE=${size} ${IMAGE_PATH} -f "${IMAGE_PATH}Dockerfile.prod" ${push_flag:+--push}
+  done
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-persistence${tag}" ${PERSISTENCE_PATH} -f "${PERSISTENCE_PATH}Dockerfile.prod" ${push_flag:+--push}
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-webui${tag}" ${WEBUI_PATH} -f "${WEBUI_PATH}Dockerfile.prod" ${push_flag:+--push}
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-auth${tag}" ${AUTH_PATH} -f "${AUTH_PATH}Dokcerfile.prod" ${push_flag:+--push}
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-recommender${tag}" ${RECOMMENDER_PATH} -f "${RECOMMENDER_PATH}Dockerfile.prod" ${push_flag:+--push}
 	docker buildx rm mybuilder
 else
   cd .. && docker compose build --no-cache
